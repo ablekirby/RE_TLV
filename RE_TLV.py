254d71ad0118a35ec353df6eaf8eff82c4bc60c4
ablekirby <20812336+ablekirby@users.noreply.github.com>
2021-09-25 14:36:51 -0400

    initial commit

---
+++ RE_TLV.py
@@ -0,0 +1,320 @@
+# A class to ingest and parse htlc strings, and make their contents available in various useful formats
+# September 25, 2021
+# Able Kirby
+
+#import os
+import json
+import base64
+
+class RE_TLV:
+    def __init__(self):
+        self.raw = []
+
+    # Simple Get methods
+    def getAmmount(self):
+        amt = ''
+        try:
+            amt = self.amt_paid_sat
+        except:
+            pass
+        return amt
+
+    def getAmmountInt(self):
+        return int(self.getAmmount())
+
+    def getDate(self):
+        date = ''
+        try:
+            date = self.settle_date_str
+        except:
+            pass
+        return date
+
+    def getFeatures(self):
+        features = ''
+        try:
+            features = self.features
+        except:
+            pass
+        return features
+
+    def getHtlcs(self):
+        htlcs = ''
+        try:
+            htlcs = self.htlcs
+        except:
+            pass
+        return htlcs
+
+
+    # START - Low level functions to parse custom records
+    def getCustomRecords(self):
+        crecords = ''
+        htlcs = self.getHtlcs()
+        try:
+            crecords = htlcs["custom_records"]
+        except:
+            pass
+        return crecords
+
+    # Normal Podcast 2.0 Record
+    def get7629169(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["7629169"]
+            crecord = base64.b64decode(crecord)
+            crecord = json.loads(crecord)
+        except:
+            pass
+        return crecord
+
+    # Sender name
+    def get7629171(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["7629171"]
+            crecord = base64.b64decode(crecord)
+            crecord = crecord.decode("utf-8")
+        except:
+            pass
+        return crecord
+
+    # Normal Pre-image record.
+    def get5482373484(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["5482373484"]
+            #crecord = base64.b64decode(crecord)
+            #crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+
+    # whatsat Message
+    def get34349334(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349334"]
+            crecord = base64.b64decode(crecord)
+            crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+    
+    # whatsat signature
+    def get34349337(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349337"]
+            #crecord = base64.b64decode(crecord)
+            #crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+
+    # whatsat sender pubkey
+    def get34349339(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349339"]
+            #crecord = base64.b64decode(crecord)
+            #crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+
+    # Thunderhub (whatsat?) sender name
+    def get34349340(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349340"]
+            crecord = base64.b64decode(crecord)
+            crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+    
+    # Thunder-Hub Content Type
+    def get34349345(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349345"]
+            crecord = base64.b64decode(crecord)
+            crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+    
+    # Thunder-Hub Request Type
+    def get34349347(self):
+        crecords = self.getCustomRecords()
+        crecord = ''
+        try:
+            crecord = crecords["34349347"]
+            crecord = base64.b64decode(crecord)
+            crecord = crecord.decode('utf-8')
+        except:
+            pass
+        return crecord
+    
+    # More High-Level commands
+    def getSenderName(self):
+        name = ""
+
+        # Check for boost-a-gram name first
+        r = self.get7629169()
+        try:
+            name = r["sender_name"]
+        except:
+            pass
+
+        if not name:
+            try:
+                name = self.get7629171()
+            except:
+                pass
+
+        # Try thunderhub / whatsat 
+        if not name:
+            try:
+                name =  self.get34349340()
+            except:
+                pass
+        
+        return name
+
+    def getSenderApp(self):
+        senderApp = ""
+
+        # Try boost-a-gram app field first
+        r7629169 = self.get7629169()
+        try:
+            senderApp = r7629169["app_name"]
+        except:
+            pass
+
+        # Try whatsat
+        if not senderApp:
+            # Is this a whatsat message?
+            if self.get34349339():
+                senderApp = "Whatsat"
+        
+        return senderApp
+
+    def getMessage(self):
+        
+        message = ""
+
+        # Try Thunderhub Style
+        try:
+            message = self.get34349334()
+        except:
+            pass
+        
+        # Try message filed
+        r7629169 = self.get7629169()
+        try:
+            message = r7629169["message"]
+        except:
+            pass
+
+        return message
+
+    def getRecordNum(self):
+        recordNum = ""
+        try:
+            recordNum = self.add_index
+        except:
+            pass
+        return recordNum
+
+    def hasMessage(self):
+        hasMessage = False
+        if len(self.getMessage()) > 0:
+            hasMessage = True
+        return hasMessage
+
+    def isBoost(self):
+        isBoost = False
+        r = self.get7629169()
+        try:
+            if r["action"] == "boost":
+                isBoost = True
+        except:
+            pass
+        return isBoost
+
+    def isStream(self):
+        isStream = False
+        r = self.get7629169()
+        try:
+            if r["action"] == "stream":
+                isStream = True
+        except:
+            pass
+        return isStream
+
+    @staticmethod
+    # Method to import CSV format records, as downloaded from RTL (Ride The Lightning)
+    def fromcsv(str):
+        self = RE_TLV()
+        self.raw = str
+
+        str = str.split(',')
+
+        #
+        self.memo = str[0]
+        self.r_preimage = str[1]
+        self.r_hash = str[2]
+        self.value = str[3]
+        self.value_msat = str[4]
+        self.settled = str[5]
+        self.creation_date = str[6]
+        self.settle_date = str[7]
+        self.payment_request = str[8]
+        self.description_hash = str[9]
+        self.expiry = str[10]
+        self.fallback_addr = str[11]
+        self.cltv_expiry = str[12]
+        self.route_hints = str[13]
+        self.private = str[14]
+        self.add_index = str[15]
+        self.settle_index = str[16]
+        self.amt_paid = str[17]
+        self.amt_paid_sat = str[18]
+        self.amt_paid_msat = str[19]
+        self.state = str[20]
+        
+
+        
+
+        # Parse semi-colon seperated JSON
+        try:
+            self.htlcs = json.loads(str[21].replace(';',',')[1:-1])
+        except:
+            self.htlcs = ""
+        try:
+            self.features = json.loads(str[22].replace(';',','))
+        except:
+            pass
+
+        self.is_keysend = str[23]
+        self.payment_addr = str[24]
+        self.creation_date_str = str[25]
+        self.settle_date_str = str[26]
+        self.btc_value = str[27]
+        self.btc_amt_paid_sat = str[28]
+
+        # Re
+        
+        return self
+
+
---
+++ RE_testTLV.py
@@ -0,0 +1,86 @@
+# A Script to test the RE_TLV class
+# September 25, 2021
+# Able Kirby
+
+# Instructions:
+# Export invoices from RTL as a .csv file [Make sure to set the max ]
+
+
+from RE_TLV import RE_TLV
+import sys
+
+# Load RTL exported records
+fn = sys.argv[1]
+with open(fn,mode='r') as file:
+    file.seek(3) # discard BOM
+    file.readline() #discard first line (header)
+    csvlines = file.readlines() 
+
+# For every record in file
+
+print("")
+appsum = []
+apps = []
+boostsum = []
+boosters = []
+sats_of_unknown_origin = 0
+unidentified_transactions = 0
+
+print("---TLV Messages---")
+for line in csvlines:
+    mytlv = RE_TLV.fromcsv(line)
+    # Coleate streaming sats by app
+    if not mytlv.isBoost():
+        app = mytlv.getSenderApp()
+        asats = mytlv.getAmmountInt()
+        if not app:
+            app = "Unknown App"
+        try:
+            ind = apps.index(app)
+            appsum[ind] += asats
+        except:
+            apps.append(app)
+            appsum.append(asats)
+
+    # Coleate Boosts by name
+    elif mytlv.isBoost():
+        booster = mytlv.getSenderName()
+        bsats = mytlv.getAmmountInt()
+        if not booster:
+            booster = "U.B.M. (Unknown Boosting Maniac)"
+        try:
+            ind = boosters.index(booster)
+            boostsum[ind] += bsats
+        except:
+            boosters.append(booster)
+            boostsum.append(bsats)
+    else:
+        sats_of_unknown_origin += mytlv.getAmmountInt()
+        unresolved_transactions += 1
+
+    if mytlv.hasMessage():
+        print("Record Number: " + mytlv.getRecordNum())
+        print("\tAmmount: " + mytlv.getAmmount() + " sats")
+        print("\tDate: " + mytlv.getDate() + " UTC")
+        print("\tSender Name: " + mytlv.getSenderName())
+        print("\tSender App: " +  mytlv.getSenderApp())
+        print("\tMessage Text: " + mytlv.getMessage())
+        print("")
+
+# Print unique list of Apps
+print("---Sats From Streaming Apps---")
+for i in range(0,len(apps)-1):
+    print(apps[i] + " = " + str(appsum[i]) + " sats")
+print("\tTotal From Streams: " + str(sum(appsum)) + " sats")
+print("")
+
+# Print unique list of Boosters
+print("---Sats From Boosters---")
+for i in range(0,len(boosters)-1):
+    print(boosters[i] + " = " + str(boostsum[i]) + " sats")
+print("\tTotal From Boosts: " + str(sum(boostsum)) + " sats")
+print("")
+
+# Print Everything else
+print("---Sats of Unidentified Origin---")
+print(str(sats_of_unknown_origin) + " sats in " + str(unidentified_transactions) + " transactions\n")
